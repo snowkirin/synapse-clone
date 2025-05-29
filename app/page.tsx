5d64bf4fcc6e3a9493921d1510b1e3bd60279fe1
정진수 <jinsoo.jung@datamaker.io>
2025-05-29 17:30:04 +0900

    prettier 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
 // import { Button } from "@/components/ui/button";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
 
 interface Todo {
   userId: number;
@@ -12,37 +12,38 @@ interface Todo {
 
 export default function Home() {
   const { data, isLoading, error } = useQuery<Todo[], Error>({
-    queryKey: ["test"],
-    queryFn: () => fetch("https://jsonplaceholder.typicode.com/todos?_limit=5").then((res) => res.json()),
+    queryKey: ['test'],
+    queryFn: () =>
+      fetch('https://jsonplaceholder.typicode.com/todos?_limit=5').then((res) =>
+        res.json()
+      ),
   });
 
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="w-full max-w-md">
-          <h1 className="text-2xl font-semibold mb-4 text-center sm:text-left">
-            Todos
-          </h1>
-          {isLoading && <p className="text-center">Loading...</p>}
-          {error && (
-            <p className="text-center text-red-500">
-              Error: {error.message}
-            </p>
-          )}
-          {data && (
-            <ul className="list-disc pl-5 space-y-2">
-              {data.map((todo) => (
-                <li
-                  key={todo.id}
-                  className={`${
-                    todo.completed ? "line-through text-gray-500" : ""
-                  }`}
-                >
-                  {todo.title}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+    <div className='flex min-h-screen flex-col items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
+      <div className='w-full max-w-md'>
+        <h1 className='text-cool-gray-500 mb-4 text-center text-2xl font-semibold sm:text-left'>
+          Todos
+        </h1>
+        {isLoading && <p className='text-center'>Loading...</p>}
+        {error && (
+          <p className='text-center text-red-500'>Error: {error.message}</p>
+        )}
+        {data && (
+          <ul className='list-disc space-y-2 pl-5'>
+            {data.map((todo) => (
+              <li
+                key={todo.id}
+                className={`${
+                  todo.completed ? 'text-gray-500 line-through' : ''
+                }`}
+              >
+                {todo.title}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
