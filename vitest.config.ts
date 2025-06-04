b5f5aa67ae9f3efc4cea62823d9db7cd7ab79f9b
정진수 <jinsoo.jung@datamaker.io>
2025-06-04 14:17:19 +0900

    Add Button component test and configure vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,6 +13,8 @@ const dirname =
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
     workspace: [
       {
         extends: true,
