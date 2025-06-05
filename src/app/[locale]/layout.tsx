fea808f78a51c703106fa38e995bc73acfda658d
정진수 <jinsoo.jung@datamaker.io>
2025-06-05 14:43:18 +0900

    fix: add ReactNode imports

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { type ReactNode } from 'react';
 import '../globals.css';
 import { TanStackQueryProvider } from '@/providers';
 
@@ -22,7 +23,7 @@ export default function RootLayout({
   children,
   params,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
   params: { locale: string };
 }>) {
   return (
